39013508d7253bee6dc32eb09697cbf985046e1f
Auriane Labille <45030838+Fanghienne@users.noreply.github.com>
2022-10-27 15:44:02 +0200

    GET users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,43 +7,43 @@
         class="list-group-item d-flex justify-content-between align-items-start"
         aria-current="true"
         v-for="user in users"
+        v-bind:key="user"
       >
-        <a href="" class="ms-2 me-auto text-decoration-none text-dark">
-          <div class="fw-bold">{{ user.name }} - {{ user.age }} ans</div>
+        <a
+          href=""
+          class="ms-2 me-auto text-decoration-none text-dark text-start"
+        >
+          <div class="fw-bold">{{ user.fullname }} - {{ user.age }} ans</div>
           {{ user.city }}
         </a>
+        <!-- <a href="" class="btn tbn-primary">Edit</a>
+        <a href="" class="btn btn-danger">Delete</a> -->
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Home",
   components: {},
   data() {
     return {
-      users: [
-        {
-          name: "Test",
-          email: "test@gmail.com",
-          age: 24,
-          city: "Mumbai",
-        },
-        {
-          name: "Test3",
-          email: "test3@gmail.com",
-          age: 35,
-          city: "Paris",
-        },
-        {
-          name: "Test2",
-          email: "test2@gmail.com",
-          age: 19,
-          city: "London",
-        },
-      ],
+      users: [],
     };
+  },
+  methods: {
+    async getUsers() {
+      await axios
+        .get(`https://connectify-api-ynov.herokuapp.com/users`)
+        .then((response) => {
+          this.users = response.data;
+        });
+    },
+  },
+  mounted() {
+    this.getUsers();
   },
 };
 </script>
