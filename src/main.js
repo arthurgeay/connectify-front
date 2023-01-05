633c6c8115f564b6f26f01947ad5160da527db6a
Auriane Labille <auriane.labille@gmail.com>
2023-01-05 13:33:14 +0100

    REMOVE Bootstrap and ADD picocss

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App);
 
