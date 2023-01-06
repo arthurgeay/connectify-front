78a1acb1731598f36627154ee9c10bf4b0de7760
Yessi yessi-y-munguengui <yessi.yessimunguengui@gmail.com>
2023-01-06 14:10:50 +0100

    feat : add soap call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,13 @@
       </button>
     </div>
 
-    <article class="d-flex justify-between" :aria-busy="this.isLoading" aria-current="true" v-for="user in users" v-bind:key="user">
+    <article
+      class="d-flex justify-between"
+      :aria-busy="this.isLoading"
+      aria-current="true"
+      v-for="user in users"
+      v-bind:key="user"
+    >
       <a>
         <div>{{ user.fullname }} - {{ user.age }} ans</div>
         {{ user.city }}
@@ -16,7 +22,10 @@
         <button @click="$router.push(`/activities/${user._id}`)">
           Activités
         </button>
-        <button class="outline contrast" @click="$router.push(`/users/${user._id}/update`)">
+        <button
+          class="outline contrast"
+          @click="$router.push(`/users/${user._id}/update`)"
+        >
           Modifier
         </button>
         <button class="outline secondary" @click="deleteUser(user._id)">
@@ -29,6 +38,26 @@
 
 <script>
 import axios from "axios";
+var data =
+  '<?xml version="1.0" encoding="utf-8"?>\n<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">\n  <soap12:Body>\n    <CelsiusToFahrenheit xmlns="https://www.w3schools.com/xml/">\n      <Celsius>20</Celsius>\n    </CelsiusToFahrenheit>\n  </soap12:Body>\n</soap12:Envelope>';
+
+var config = {
+  method: "post",
+  url: "https://www.w3schools.com/xml/tempconvert.asmx",
+  headers: {
+    "Content-Type": "application/soap+xml; charset=utf-8",
+    "Access-Control-Allow-Origin": "*",
+  },
+  data: data,
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 export default {
   name: "Home",
@@ -63,12 +92,23 @@ export default {
           await this.getUsers();
         });
     },
+
+    async soapApiCall() {
+      await axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
   },
   mounted() {
     if (!localStorage.getItem("token") && !localStorage.getItem("user")) {
       return this.$router.push("/login");
     }
     this.getUsers();
+    this.soapApiCall();
   },
 };
 </script>
