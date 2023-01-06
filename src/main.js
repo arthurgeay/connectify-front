6c207b34aeaa16b2f2ea0660ee9be32c949b10d8
Auriane Labille <auriane.labille@gmail.com>
2023-01-06 14:37:53 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import axios from "axios";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import "./assets/style.css"
 
 const app = createApp(App);
 
