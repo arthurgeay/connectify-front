cd32e94ab99d34fa4794c3467ae2ef96f2e0c3c7
Auriane Labille <auriane.labille@gmail.com>
2023-01-12 11:28:33 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,54 +1,20 @@
 <template>
-  <div class="container">
-    <div
-      class="row justify-content-center align-content-center"
-      style="height: 100vh"
-    >
-      <h1 class="mb-5 text-center" style="height: fit-content">Sign in</h1>
-      <form class="text-start w-50">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label"
-            >Email address</label
-          >
-          <input
-            type="email"
-            v-model="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            required
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input
-            type="password"
-            autocomplete="on"
-            v-model="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            required
-          />
-        </div>
-        <div class="d-flex flex-column">
-          <button
-            type="submit"
-            class="btn btn-primary mb-3"
-            v-on:click.prevent="login()"
-          >
-            Login
-          </button>
-          <p class="text-center">
-            Not a member?
-            <router-link to="/registration">Register</router-link>
-          </p>
-        </div>
-      </form>
-    </div>
-  </div>
+
+  <h1>Sign in</h1>
+  <form>
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" v-model="email" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+    <small id="emailHelp">We'll never share your email with anyone else.</small>
+
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" autocomplete="on" v-model="password" id="exampleInputPassword1" required/>
+    
+    <button type="submit" v-on:click.prevent="login()">Login</button>
+    <p>
+      Not a member?
+      <router-link to="/registration">Register</router-link>
+    </p>
+  </form>
 </template>
 
 <script>
